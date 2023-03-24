b4484cfa1d592a959f6f8181abe51d9e1be61e03
Aniq Ali <axa210345@utdallas.edu>
2023-03-24 18:54:44 -0500

    Remove

--- app/Exceptions/UnsupportedLanguageExceptions.py
+++
@@ -1,5 +0,0 @@
-supported_languages = list[str] = ['en','es']
-
-class UnsupportedLanguageException(Exception):
-    def __init__(self, lang: str) -> None:
-        super().__init__(*args)
--- app/utils/utils.py
+++
@@ -1,7 +0,0 @@
-def greet(firstname: str, lastname: str, lang: str = 'en') -> str:
-    if lang == 'en':
-        return f'Hello, {firstname} {lastname}'
-    elif lang =='es':
-        return f'Hola, {firstname} {lastname}'
-    else:
-        raise UnsupportedLanguageExceptions
